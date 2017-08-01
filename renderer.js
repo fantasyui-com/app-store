a14b37bb4bbc8d62346a1cd7f7ecd5c59fc03a24
Captain Fantasy <captain@fantasyui.com>
2017-08-01 11:50:45 -0400

    Vue Integration and basic models

--- renderer.js
+++ renderer.js
@@ -1,3 +1,39 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+
+var appTitle = new Vue({
+  el: '#app-title',
+  data: {
+    name: 'App Store',
+    description: 'Manage Electron Applications'
+  }
+});
+
+
+var appPackages = new Vue({
+  el: '#app-packages',
+
+  data: {
+    packages: [
+
+      { name: 'Bootstrap Electron 1', description: 'Bootstrap based Electron Application Starter Pack', buttonLabel:'Install', progressBar:false, },
+      { name: 'Bootstrap Electron 2', description: 'Bootstrap based Electron Application Starter Pack', buttonLabel:'Install', progressBar:false, },
+      { name: 'Bootstrap Electron 3', description: 'Bootstrap based Electron Application Starter Pack', buttonLabel:'Install', progressBar:false, },
+      { name: 'Bootstrap Electron 4', description: 'Bootstrap based Electron Application Starter Pack', buttonLabel:'Install', progressBar:false, },
+
+    ]
+  },
+
+  methods: {
+    install: function (pkg) {
+
+      console.log(pkg.name);
+      pkg.buttonLabel = 'Installing';
+      pkg.progressBar = true;
+
+
+    }
+  }
+})
